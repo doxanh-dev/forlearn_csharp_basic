5cc33ccd2415a7b8056a85a730e8b1b16777b4a6
Nguyen Kim Tu <tunk@doxanh.edu.vn>
2019-04-15 23:28:14 +0700

    em commit baitap003 của C#

---
+++ TuNK/baitap003/baitap003/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace baitap003
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtShowWeb = new System.Windows.Forms.RichTextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.listWeb = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(216, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Liên Kết Website";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(73, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bạn hãy chọn site";
+            // 
+            // txtShowWeb
+            // 
+            this.txtShowWeb.Enabled = false;
+            this.txtShowWeb.Location = new System.Drawing.Point(347, 152);
+            this.txtShowWeb.Name = "txtShowWeb";
+            this.txtShowWeb.Size = new System.Drawing.Size(246, 116);
+            this.txtShowWeb.TabIndex = 3;
+            this.txtShowWeb.Text = "";
+            this.txtShowWeb.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.txtShowWeb_LinkClicked);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnOK.Location = new System.Drawing.Point(65, 301);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(129, 47);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnReset.Location = new System.Drawing.Point(265, 301);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(129, 47);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnThoat.Location = new System.Drawing.Point(464, 301);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(129, 47);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // listWeb
+            // 
+            this.listWeb.FormattingEnabled = true;
+            this.listWeb.ItemHeight = 16;
+            this.listWeb.Location = new System.Drawing.Point(65, 152);
+            this.listWeb.Name = "listWeb";
+            this.listWeb.Size = new System.Drawing.Size(246, 116);
+            this.listWeb.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 386);
+            this.Controls.Add(this.listWeb);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtShowWeb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Liên kết Web";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtShowWeb;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ListBox listWeb;
+    }
+}
+
