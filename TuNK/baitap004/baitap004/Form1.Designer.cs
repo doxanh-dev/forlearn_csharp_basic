f86456e714df33fedfd445a7159c54d35d95bcc6
Nguyen Kim Tu <tunk@saisystem.vn>
2019-04-16 14:36:32 +0700

    em commit baitap004 giai phuong trinh

--- TuNK/baitap004/baitap004/Form1.Designer.cs
+++ TuNK/baitap004/baitap004/Form1.Designer.cs
@@ -50,9 +50,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(119, 31);
+            this.label1.Location = new System.Drawing.Point(89, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(254, 35);
+            this.label1.Size = new System.Drawing.Size(205, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Giải Phương Trình";
             // 
@@ -61,9 +62,11 @@
             this.groupBox1.Controls.Add(this.radioSelect2);
             this.groupBox1.Controls.Add(this.radioSelect1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.groupBox1.Location = new System.Drawing.Point(53, 88);
+            this.groupBox1.Location = new System.Drawing.Point(40, 66);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(379, 128);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(284, 96);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Bạn vui lòng chọn";
@@ -72,9 +75,10 @@
             // 
             this.radioSelect2.AutoSize = true;
             this.radioSelect2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.radioSelect2.Location = new System.Drawing.Point(37, 80);
+            this.radioSelect2.Location = new System.Drawing.Point(28, 60);
+            this.radioSelect2.Margin = new System.Windows.Forms.Padding(2);
             this.radioSelect2.Name = "radioSelect2";
-            this.radioSelect2.Size = new System.Drawing.Size(210, 24);
+            this.radioSelect2.Size = new System.Drawing.Size(180, 21);
             this.radioSelect2.TabIndex = 1;
             this.radioSelect2.TabStop = true;
             this.radioSelect2.Text = "Giải Phương Trình bậc 2";
@@ -85,9 +89,10 @@
             // 
             this.radioSelect1.AutoSize = true;
             this.radioSelect1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.radioSelect1.Location = new System.Drawing.Point(37, 45);
+            this.radioSelect1.Location = new System.Drawing.Point(28, 34);
+            this.radioSelect1.Margin = new System.Windows.Forms.Padding(2);
             this.radioSelect1.Name = "radioSelect1";
-            this.radioSelect1.Size = new System.Drawing.Size(210, 24);
+            this.radioSelect1.Size = new System.Drawing.Size(180, 21);
             this.radioSelect1.TabIndex = 0;
             this.radioSelect1.TabStop = true;
             this.radioSelect1.Text = "Giải Phương Trình bậc 1";
@@ -98,27 +103,31 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label2.Location = new System.Drawing.Point(38, 241);
+            this.label2.Location = new System.Drawing.Point(28, 181);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 24);
+            this.label2.Size = new System.Drawing.Size(63, 18);
             this.label2.TabIndex = 2;
             this.label2.Text = "Nhập a: ";
             // 
             // txtA
             // 
             this.txtA.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtA.Location = new System.Drawing.Point(125, 241);
+            this.txtA.Location = new System.Drawing.Point(94, 181);
+            this.txtA.Margin = new System.Windows.Forms.Padding(2);
             this.txtA.Name = "txtA";
-            this.txtA.Size = new System.Drawing.Size(161, 28);
+            this.txtA.Size = new System.Drawing.Size(122, 24);
             this.txtA.TabIndex = 3;
+            this.txtA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtA_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label3.Location = new System.Drawing.Point(38, 324);
+            this.label3.Location = new System.Drawing.Point(28, 242);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 24);
+            this.label3.Size = new System.Drawing.Size(63, 18);
             this.label3.TabIndex = 4;
             this.label3.Text = "Nhập c: ";
             // 
@@ -126,9 +135,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label4.Location = new System.Drawing.Point(38, 284);
+            this.label4.Location = new System.Drawing.Point(28, 213);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 24);
+            this.label4.Size = new System.Drawing.Size(63, 18);
             this.label4.TabIndex = 5;
             this.label4.Text = "Nhập b: ";
             // 
@@ -136,25 +146,30 @@
             // 
             this.txtC.Enabled = false;
             this.txtC.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtC.Location = new System.Drawing.Point(125, 319);
+            this.txtC.Location = new System.Drawing.Point(94, 239);
+            this.txtC.Margin = new System.Windows.Forms.Padding(2);
             this.txtC.Name = "txtC";
-            this.txtC.Size = new System.Drawing.Size(161, 28);
+            this.txtC.Size = new System.Drawing.Size(122, 24);
             this.txtC.TabIndex = 6;
+            this.txtC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtC_TextChanged);
             // 
             // txtB
             // 
             this.txtB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtB.Location = new System.Drawing.Point(125, 281);
+            this.txtB.Location = new System.Drawing.Point(94, 211);
+            this.txtB.Margin = new System.Windows.Forms.Padding(2);
             this.txtB.Name = "txtB";
-            this.txtB.Size = new System.Drawing.Size(161, 28);
+            this.txtB.Size = new System.Drawing.Size(122, 24);
             this.txtB.TabIndex = 7;
+            this.txtB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtB_TextChanged);
             // 
             // btnGiai
             // 
             this.btnGiai.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btnGiai.Location = new System.Drawing.Point(332, 241);
+            this.btnGiai.Location = new System.Drawing.Point(249, 181);
+            this.btnGiai.Margin = new System.Windows.Forms.Padding(2);
             this.btnGiai.Name = "btnGiai";
-            this.btnGiai.Size = new System.Drawing.Size(113, 68);
+            this.btnGiai.Size = new System.Drawing.Size(85, 51);
             this.btnGiai.TabIndex = 8;
             this.btnGiai.Text = "Giải";
             this.btnGiai.UseVisualStyleBackColor = true;
@@ -163,9 +178,10 @@
             // btnThoat
             // 
             this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btnThoat.Location = new System.Drawing.Point(332, 319);
+            this.btnThoat.Location = new System.Drawing.Point(249, 239);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(2);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(113, 29);
+            this.btnThoat.Size = new System.Drawing.Size(85, 24);
             this.btnThoat.TabIndex = 9;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
@@ -175,9 +191,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label5.Location = new System.Drawing.Point(39, 377);
+            this.label5.Location = new System.Drawing.Point(29, 283);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(84, 24);
+            this.label5.Size = new System.Drawing.Size(66, 18);
             this.label5.TabIndex = 10;
             this.label5.Text = "Kết quả: ";
             // 
@@ -185,16 +202,17 @@
             // 
             this.txtResule.Enabled = false;
             this.txtResule.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtResule.Location = new System.Drawing.Point(125, 374);
+            this.txtResule.Location = new System.Drawing.Point(94, 280);
+            this.txtResule.Margin = new System.Windows.Forms.Padding(2);
             this.txtResule.Name = "txtResule";
-            this.txtResule.Size = new System.Drawing.Size(320, 28);
+            this.txtResule.Size = new System.Drawing.Size(241, 24);
             this.txtResule.TabIndex = 11;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 450);
+            this.ClientSize = new System.Drawing.Size(370, 338);
             this.Controls.Add(this.txtResule);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.btnThoat);
@@ -208,6 +226,7 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
--- TuNK/baitap004/baitap004/Form1.cs
+++ TuNK/baitap004/baitap004/Form1.cs
@@ -17,11 +17,6 @@ namespace baitap004
             InitializeComponent();
         }
 
-        private void btnGiai_Click(object sender, EventArgs e)
-        {
-           
-        }
-
         private void radioSelect1_CheckedChanged(object sender, EventArgs e)
         {
             txtC.Enabled = false;
@@ -32,12 +27,135 @@ namespace baitap004
             txtC.Enabled = true;
         }
 
+        private void txtA_TextChanged(object sender, KeyPressEventArgs e)
+        {
+            CheckOnlyNumber(sender, e);
+        }
+
+        private void txtB_TextChanged(object sender, KeyPressEventArgs e)
+        {
+            CheckOnlyNumber(sender, e);
+        }
+
+        private void txtC_TextChanged(object sender, KeyPressEventArgs e)
+        {
+            CheckOnlyNumber(sender, e);
+        }
+
+        //Sự kiên khi click button Giải
+        private void btnGiai_Click(object sender, EventArgs e)
+        {
+            var a = txtA.Text;
+            var b = txtB.Text;
+            var c = txtC.Text;
+
+            if (string.IsNullOrEmpty(a) || string.IsNullOrEmpty(b))
+            {
+                MessageBox.Show("Các tham số cần phải điền đầy đủ !");
+                if (string.IsNullOrEmpty(a))
+                {
+                    txtA.Focus();
+                }
+                else
+                {
+                    txtB.Focus();
+                }
+            }
+            else
+            {
+                if (radioSelect1.Checked == true)
+                {
+                    GiaiPhuongTrinhBac1(float.Parse(a), float.Parse(b));
+                }
+                else if (radioSelect2.Checked == true)
+                {
+                    if (string.IsNullOrEmpty(c))
+                    {
+                        MessageBox.Show("Các tham số cần phải điền đầy đủ !");
+                        txtC.Focus();
+                    }
+                    else
+                    {
+                        GiaiPhuongTrinhBac2(float.Parse(a), float.Parse(b), float.Parse(c));
+                    }
+                }
+            }
+        }
+
+        //GiaiPhuongTrinhBac1 
+        private void GiaiPhuongTrinhBac1(float a, float b)
+        {
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    txtResule.Text = "Pt vô số nghiệm";
+                }
+                else
+                {
+                    txtResule.Text = "Pt vô nghiệm";
+                }
+            }
+            else 
+            {
+                var result = -b/a;
+
+                float x = (float)Math.Round(result * 100f) / 100f;
+                txtResule.Text = "Pt có nghiệm: "+ x;
+            }
+        }
+
+        //GiaiPhuongTrinhBac2
+        private void GiaiPhuongTrinhBac2(float a, float b, float c)
+        {
+            var delta = (b * b) - (4 * a * c);
+
+            if (delta < 0)
+            {
+                txtResule.Text = "Pt vô nghiệm";
+            }
+            else if (delta == 0)
+            {
+                var result = (-b) / (2 * a);
+                txtResule.Text = "Pt có nghiệm kép: x1=x1=" + result;
+            }
+            else
+            {
+                var x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                var x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                float x1c = (float)Math.Round(x1 * 100f) / 100f;
+                float x2c = (float)Math.Round(x2 * 100f) / 100f;
+                txtResule.Text = "Pt có 2 nghiệm: x1=" + x1c + " và x2=" + x2c;
+            }
+        }
+
         private void btnThoat_Click(object sender, EventArgs e)
         {
             DialogResult result = MessageBox.Show("Bạn có muốn thoát hay không ?", "Chú ý", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
             if (result == DialogResult.Yes)
             {
                 Application.Exit();
+            }
+        }
+
+        //check only number
+        private void CheckOnlyNumber(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.' && e.KeyChar != '-')
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if (e.KeyChar == '.' && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '-' && (sender as TextBox).Text.IndexOf('-') > -1)
+            {
+                e.Handled = true;
             }
         }
     }
