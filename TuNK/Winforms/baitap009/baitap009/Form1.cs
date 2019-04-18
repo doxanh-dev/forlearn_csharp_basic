97d61c58f7898cd0bbc852479dbf9fd91de0a63b
Nguyen Kim Tu <tunk@saisystem.vn>
2019-04-18 15:21:45 +0700

    em update cho bt009

--- TuNK/Winforms/baitap009/baitap009/Form1.cs
+++ TuNK/Winforms/baitap009/baitap009/Form1.cs
@@ -87,13 +87,13 @@ namespace baitap009
         //check only number for TextBox
         private void CheckOnlyNumber(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '-')
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)  && e.KeyChar != '-')
             {
                 e.Handled = true;
             }
 
-            // only allow one decimal point
-            if (e.KeyChar == '-' && (sender as TextBox).Text.IndexOf('-') > -1)
+            // only allow minus sign at the beginning
+            if (e.KeyChar == '-' && (sender as TextBox).Text.Length > 0)
             {
                 e.Handled = true;
             }
