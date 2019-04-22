bfefb94927b60a38dfb7acabfb45d8762b532b7f
Nguyen Kim Tu <tunk@saisystem.vn>
2019-04-22 11:21:13 +0700

    em commit thêm bt 012: thêm dãy số và tính tổng

---
+++ TuNK/Winforms/baitap012/baitap012/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace baitap012
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnNhap = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDaySo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTong = new System.Windows.Forms.TextBox();
+            this.txtTongChan = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTongLe = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnTiepTuc = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(43, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập Dãy Số Và Tính Tổng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(44, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập số";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtNum.Location = new System.Drawing.Point(179, 81);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(70, 23);
+            this.txtNum.TabIndex = 2;
+            this.txtNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNum_KeyPress);
+            // 
+            // btnNhap
+            // 
+            this.btnNhap.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNhap.Location = new System.Drawing.Point(255, 78);
+            this.btnNhap.Name = "btnNhap";
+            this.btnNhap.Size = new System.Drawing.Size(62, 29);
+            this.btnNhap.TabIndex = 3;
+            this.btnNhap.Text = "Nhập";
+            this.btnNhap.UseVisualStyleBackColor = true;
+            this.btnNhap.Click += new System.EventHandler(this.btnNhap_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(44, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Dãy vừa nhập";
+            // 
+            // txtDaySo
+            // 
+            this.txtDaySo.Enabled = false;
+            this.txtDaySo.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtDaySo.Location = new System.Drawing.Point(148, 114);
+            this.txtDaySo.Name = "txtDaySo";
+            this.txtDaySo.Size = new System.Drawing.Size(169, 23);
+            this.txtDaySo.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(44, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tổng các phần tử trong dãy";
+            // 
+            // txtTong
+            // 
+            this.txtTong.Enabled = false;
+            this.txtTong.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtTong.Location = new System.Drawing.Point(255, 145);
+            this.txtTong.Name = "txtTong";
+            this.txtTong.Size = new System.Drawing.Size(62, 23);
+            this.txtTong.TabIndex = 7;
+            // 
+            // txtTongChan
+            // 
+            this.txtTongChan.Enabled = false;
+            this.txtTongChan.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtTongChan.Location = new System.Drawing.Point(125, 177);
+            this.txtTongChan.Name = "txtTongChan";
+            this.txtTongChan.Size = new System.Drawing.Size(45, 23);
+            this.txtTongChan.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(44, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tổng chẵn";
+            // 
+            // txtTongLe
+            // 
+            this.txtTongLe.Enabled = false;
+            this.txtTongLe.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtTongLe.Location = new System.Drawing.Point(272, 177);
+            this.txtTongLe.Name = "txtTongLe";
+            this.txtTongLe.Size = new System.Drawing.Size(45, 23);
+            this.txtTongLe.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(212, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Tổng lẻ";
+            // 
+            // btnTiepTuc
+            // 
+            this.btnTiepTuc.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnTiepTuc.Location = new System.Drawing.Point(84, 219);
+            this.btnTiepTuc.Name = "btnTiepTuc";
+            this.btnTiepTuc.Size = new System.Drawing.Size(80, 35);
+            this.btnTiepTuc.TabIndex = 14;
+            this.btnTiepTuc.Text = "Tiếp tục";
+            this.btnTiepTuc.UseVisualStyleBackColor = true;
+            this.btnTiepTuc.Click += new System.EventHandler(this.btnTiepTuc_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnThoat.Location = new System.Drawing.Point(201, 219);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(80, 35);
+            this.btnThoat.TabIndex = 15;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 275);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnTiepTuc);
+            this.Controls.Add(this.txtTongLe);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtTongChan);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTong);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDaySo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnNhap);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dãy số và tính tổng";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnNhap;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDaySo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTong;
+        private System.Windows.Forms.TextBox txtTongChan;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTongLe;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnTiepTuc;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ TuNK/Winforms/baitap012/baitap012/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitap012
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            checkOnlyNumber(sender, e);
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            var number = txtNum.Text;
+            var daySo = txtDaySo.Text;
+            int tong = !string.IsNullOrEmpty(txtTong.Text) ? int.Parse(txtTong.Text) : 0;
+            int tongChan = !string.IsNullOrEmpty(txtTongChan.Text) ? int.Parse(txtTongChan.Text) : 0;
+            int tongLe = !string.IsNullOrEmpty(txtTongLe.Text) ? int.Parse(txtTongLe.Text) : 0;
+
+            if (string.IsNullOrEmpty(number))
+            {
+                MessageBox.Show("Bạn chưa điền số ", "Thông báo");
+                txtNum.Focus();
+            }
+            else
+            {
+                //hien thi day so
+                daySo += number + " ";
+                txtDaySo.Text = daySo;
+
+                //tinh tong
+                tong += int.Parse(number);
+                txtTong.Text = tong.ToString();
+
+                //tinh tong so chan va so le
+                if (int.Parse(number)%2 == 0)
+                {
+                    tongChan += int.Parse(number);
+                }
+                else
+                {
+                    tongLe += int.Parse(number);
+                }
+
+                //hien thi tong so Chan va tong so Le
+                txtTongChan.Text = tongChan.ToString();
+                txtTongLe.Text = tongLe.ToString();
+            }
+        }
+
+        private void btnTiepTuc_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "";
+            txtDaySo.Text = "";
+            txtTong.Text = "";
+            txtTongChan.Text = "";
+            txtTongLe.Text = "";
+            txtNum.Focus();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn thoát hay không?", "Chú ý", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        //check only number
+        private void checkOnlyNumber(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+            //// only allow one decimal point
+            //if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            //{
+            //    e.Handled = true;
+            //}
+        }
+    }
+}
