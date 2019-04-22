1a255bc808e47579bc3e4a14b532b6c5d13144aa
Nguyen Kim Tu <tunk@saisystem.vn>
2019-04-22 14:27:57 +0700

    em them baitap 014 kiem tra so nguyen to

---
+++ TuNK/Winforms/baitap014/baitap014/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace baitap014
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnKiemTra = new System.Windows.Forms.Button();
+            this.txtKiemTra = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtListSNT = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(78, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(314, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kiểm Tra và Tìm Số Nguyên Tố";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(115, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập n:";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtNum.Location = new System.Drawing.Point(178, 70);
+            this.txtNum.MaxLength = 2;
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(75, 23);
+            this.txtNum.TabIndex = 2;
+            this.txtNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNum_KeyPress);
+            // 
+            // btnKiemTra
+            // 
+            this.btnKiemTra.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnKiemTra.Location = new System.Drawing.Point(269, 68);
+            this.btnKiemTra.Name = "btnKiemTra";
+            this.btnKiemTra.Size = new System.Drawing.Size(86, 27);
+            this.btnKiemTra.TabIndex = 3;
+            this.btnKiemTra.Text = "Kiểm Tra";
+            this.btnKiemTra.UseVisualStyleBackColor = true;
+            this.btnKiemTra.Click += new System.EventHandler(this.btnKiemTra_Click);
+            // 
+            // txtKiemTra
+            // 
+            this.txtKiemTra.Enabled = false;
+            this.txtKiemTra.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtKiemTra.Location = new System.Drawing.Point(178, 104);
+            this.txtKiemTra.Name = "txtKiemTra";
+            this.txtKiemTra.Size = new System.Drawing.Size(237, 23);
+            this.txtKiemTra.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(70, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kiểm tra SNT:";
+            // 
+            // txtListSNT
+            // 
+            this.txtListSNT.Enabled = false;
+            this.txtListSNT.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtListSNT.Location = new System.Drawing.Point(178, 135);
+            this.txtListSNT.Name = "txtListSNT";
+            this.txtListSNT.Size = new System.Drawing.Size(237, 23);
+            this.txtListSNT.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(32, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tìm SNT nhỏ hơn n:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 185);
+            this.Controls.Add(this.txtListSNT);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtKiemTra);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnKiemTra);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Số nguyên tố";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnKiemTra;
+        private System.Windows.Forms.TextBox txtKiemTra;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtListSNT;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TuNK/Winforms/baitap014/baitap014/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitap014
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            checkOnlyNumber(sender, e);
+        }
+
+        private void btnKiemTra_Click(object sender, EventArgs e)
+        {
+            var num = txtNum.Text;
+
+            if (string.IsNullOrEmpty(num))
+            {
+                MessageBox.Show("Bạn chưa nhập số !!!", "Thông báo");
+                txtNum.Focus();
+                txtKiemTra.Text = "";
+                txtListSNT.Text = "";
+            }
+            else
+            {
+                if (kiemTraSNT(int.Parse(num)))
+                {
+                    txtKiemTra.Text = num + " là số nguyên tố";
+                }
+                else
+                {
+                    txtKiemTra.Text = num + " không phải là số nguyên tố";
+                }
+
+                //tim day SNT 
+                timDaySNT(int.Parse(num));
+            }
+        }
+
+        //Kiem tra SNT
+        private bool kiemTraSNT(int num)
+        {
+            var result = false;
+            int count = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 2)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        //tim day SNT 
+        private void timDaySNT(int num)
+        {
+            string daySNT = "1 ";
+
+            for (int i = 1; i< num; i++)
+            {
+                if (kiemTraSNT(i))
+                {
+                    daySNT += i + " ";
+                }
+            }
+            txtListSNT.Text = daySNT;
+        }
+
+        //Check only number
+        private void checkOnlyNumber(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
