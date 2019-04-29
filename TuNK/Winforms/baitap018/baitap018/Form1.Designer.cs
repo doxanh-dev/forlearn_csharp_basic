91eaaa2ec4aa7a4a28dc29007028eeeb38635a2b
Nguyen Kim Tu <tunk@doxanh.edu.vn>
2019-04-30 00:25:29 +0700

    em commit thêm bt018 Chuyên đổi chữ hoa chữ thường

---
+++ TuNK/Winforms/baitap018/baitap018/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace baitap018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNoiDung = new System.Windows.Forms.TextBox();
+            this.rdoChuThuong = new System.Windows.Forms.RadioButton();
+            this.rdoChuHoa = new System.Windows.Forms.RadioButton();
+            this.btnKetQua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(100, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chữ Hoa và Chữ Thường";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(147, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mời bạn nhập nội dung";
+            // 
+            // txtNoiDung
+            // 
+            this.txtNoiDung.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtNoiDung.Location = new System.Drawing.Point(49, 125);
+            this.txtNoiDung.Name = "txtNoiDung";
+            this.txtNoiDung.Size = new System.Drawing.Size(363, 30);
+            this.txtNoiDung.TabIndex = 2;
+            // 
+            // rdoChuThuong
+            // 
+            this.rdoChuThuong.AutoSize = true;
+            this.rdoChuThuong.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.rdoChuThuong.Location = new System.Drawing.Point(94, 175);
+            this.rdoChuThuong.Name = "rdoChuThuong";
+            this.rdoChuThuong.Size = new System.Drawing.Size(130, 26);
+            this.rdoChuThuong.TabIndex = 3;
+            this.rdoChuThuong.TabStop = true;
+            this.rdoChuThuong.Text = "Chữ Thường";
+            this.rdoChuThuong.UseVisualStyleBackColor = true;
+            // 
+            // rdoChuHoa
+            // 
+            this.rdoChuHoa.AutoSize = true;
+            this.rdoChuHoa.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.rdoChuHoa.Location = new System.Drawing.Point(246, 175);
+            this.rdoChuHoa.Name = "rdoChuHoa";
+            this.rdoChuHoa.Size = new System.Drawing.Size(102, 26);
+            this.rdoChuHoa.TabIndex = 4;
+            this.rdoChuHoa.TabStop = true;
+            this.rdoChuHoa.Text = "Chữ Hoa";
+            this.rdoChuHoa.UseVisualStyleBackColor = true;
+            // 
+            // btnKetQua
+            // 
+            this.btnKetQua.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnKetQua.Location = new System.Drawing.Point(49, 215);
+            this.btnKetQua.Name = "btnKetQua";
+            this.btnKetQua.Size = new System.Drawing.Size(117, 43);
+            this.btnKetQua.TabIndex = 5;
+            this.btnKetQua.Text = "Kết quả";
+            this.btnKetQua.UseVisualStyleBackColor = true;
+            this.btnKetQua.Click += new System.EventHandler(this.btnKetQua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnXoa.Location = new System.Drawing.Point(172, 215);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(117, 43);
+            this.btnXoa.TabIndex = 6;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnThoat.Location = new System.Drawing.Point(295, 215);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(117, 43);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // txtShow
+            // 
+            this.txtShow.Enabled = false;
+            this.txtShow.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtShow.Location = new System.Drawing.Point(49, 274);
+            this.txtShow.Name = "txtShow";
+            this.txtShow.Size = new System.Drawing.Size(363, 30);
+            this.txtShow.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 340);
+            this.Controls.Add(this.txtShow);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnKetQua);
+            this.Controls.Add(this.rdoChuHoa);
+            this.Controls.Add(this.rdoChuThuong);
+            this.Controls.Add(this.txtNoiDung);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chữ Hoa Chữ Thường";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNoiDung;
+        private System.Windows.Forms.RadioButton rdoChuThuong;
+        private System.Windows.Forms.RadioButton rdoChuHoa;
+        private System.Windows.Forms.Button btnKetQua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.TextBox txtShow;
+    }
+}
+
---
+++ TuNK/Winforms/baitap018/baitap018/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitap018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKetQua_Click(object sender, EventArgs e)
+        {
+            var noiDung = txtNoiDung.Text;
+            var temp = string.Empty;
+
+            if (string.IsNullOrEmpty(noiDung))
+            {
+                MessageBox.Show("Bạn chưa điền nội dung !!!", "Thông báo");
+                txtNoiDung.Focus();
+            }
+            else
+            {
+                if (!rdoChuThuong.Checked && !rdoChuHoa.Checked)
+                {
+                    MessageBox.Show("Bạn chưa chọn chữ thường hoặc chữ hoa !!!", "Thông báo");
+                    rdoChuThuong.Focus();
+                }
+                else
+                {
+                    if (rdoChuThuong.Checked)
+                    {
+                        temp = noiDung.ToLower();
+                    }
+                    else
+                    {
+                        temp = noiDung.ToUpper();
+                    }
+                    txtShow.Text = temp;
+                }
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            txtNoiDung.Text = "";
+            txtShow.Text = "";
+            rdoChuHoa.Checked = false;
+            rdoChuThuong.Checked = false;
+            txtNoiDung.Focus();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn thoát không?", "Chú ý", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
